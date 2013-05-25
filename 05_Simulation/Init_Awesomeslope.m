fc374e275c56453fc63e01c84229e0293b645433
Lukas Steinert <lukas@steinert-ese.de>
2013-05-25 02:24:19 +0200

    Bugfixing in statemachine and improvements to pattern decision

--- 05_Simulation/Init_Awesomeslope.m
+++ 05_Simulation/Init_Awesomeslope.m
@@ -1,7 +1,7 @@
 %% Init the workspace variables and parameters
 function Init_Awesomeslope()
 
-assignin('base','C_int',550e-12);
+assignin('base','C_int',470e-12);
 
 assignin('base','R_pos',8000);
 assignin('base','R_neg',8000);
@@ -17,8 +17,9 @@ assignin('base','V_th_neg',-5);
 %Make the pattern such that we can fit 3 ADC samples in the smallest
 %pattern part (10% of the pattern)
 assignin('base','t_pattern',(3*625e-9)*10);
+assignin('base','meas_period',0.0025);
 
-assignin('base','SimTime_Max',0.0025);
+assignin('base','SimTime_Max',0.0025*31);
 
 assignin('base','FPGA_Output_Freq',80000000);
 
